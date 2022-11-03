50540e3def823bf21f832c4487f5a960e590d951
huixuanxu <103717620+huixuanxu@users.noreply.github.com>
2022-11-04 01:00:46 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(()=>{
+    $('.person_bella').click(function(){
+        $('.person_information_bella').slideToggle();
+});
+
+    $('.person_chara').click(function(){
+        $('.person_information_chara').slideToggle();
+});
+
+
+})
